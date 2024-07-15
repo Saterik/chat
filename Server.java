428be16a65635afc9ba2dcdc568cfd882df76641
warfa@LAPTOP-BGKII3AT <warfacesdr@mail.ru>
2024-07-16 00:24:53 +0800

    end

---
+++ Server.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void runServer(){
+        try{
+            while (!serverSocket.isClosed()){
+               Socket socket = serverSocket.accept();
+               org.example.ClienManager clienManager = new org.example.ClienManager(socket);
+               System.out.println("подключен новый клиент!");
+               Thread thread = new Thread(clienManager);
+               thread.start();
+            }
+
+        }catch (IOException e){
+            closeSocket();
+        }
+
+    }
+    private void closeSocket(){
+        try{
+            if (serverSocket != null) serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
